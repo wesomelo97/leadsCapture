8a0674240c342a2af51367270588769c02b75a98
Ewerton <eunnot@gmail.com>
2024-09-19 14:10:01 -0300

    commit de adição do tema escuro da page

---
+++ js/theme.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('theme-toggle');
+    const themeIcon = themeToggle.querySelector('i');
+    const body = document.body;
+
+    themeToggle.addEventListener('click', function() {
+        body.classList.toggle('dark-theme');
+        
+        if (body.classList.contains('dark-theme')) {
+            themeIcon.classList.remove('bi-sun');
+            themeIcon.classList.add('bi-moon');
+        } else {
+            themeIcon.classList.remove('bi-moon');
+            themeIcon.classList.add('bi-sun');
+        }
+    });
+});
